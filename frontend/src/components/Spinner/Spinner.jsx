1eeebb663990309eb61f5ca40cceb4a8a25a6046
AKSHAT GUPTA <gemini.akshatgupta@gmail.com>
2025-05-23 05:37:41 +0530

    Added Spinner

---
+++ frontend/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Spin from "../../assests/Spinner@1x-0.9s-113px-113px.gif"
+
+const Spinner = () => {
+  return (
+    <div>
+        <img src={Spin} alt="" />
+    </div>
+  )
+}
+
+export default Spinner
--- frontend/src/pages/SearchPage/searchPage.jsx
+++ frontend/src/pages/SearchPage/searchPage.jsx
@@ -3,6 +3,7 @@ import CardContainer from '../../components/CardContainer/CardContainerAdd'
 import './searchPage.css'
 import { useState } from 'react'
 import apiFetch from '../../api/apiFetch'
+import Spinner from '../../components/Spinner/Spinner'
 
 
 const SearchPage = () => {
@@ -10,12 +11,15 @@ const SearchPage = () => {
     // console.log(key);
 
     // const key = "AIzaSyBX2Err3hD3JST_XmxqpJ5anLA-9VC2nZ8"
+    const [spinner, setspinner] = useState(false)
     const [text, setText] = useState('');
     const [books, setBooks] = useState();
 
     const searchResults = async () => {
+        setspinner(true)
         const articles = await apiFetch(`https://www.googleapis.com/books/v1/volumes?q=` + text.replace(' ', '+') + `&key=` + key + '&maxResults=40')
         // await setBooks(articles)
+        setspinner(false)
         setBooks(articles)
         // await console.log(books)
     }
@@ -26,7 +30,8 @@ const SearchPage = () => {
                 <input type="text" placeholder='Search' onKeyDown={(e) => {if (e.key === 'Enter') {searchResults()}}} onChange={(e) => setText(e.target.value)} className='searchbox' value={text}/>
                 <button onClick={() => { searchResults() }} className='searchbtn'>Search</button>
             </div>
-            {books ? <CardContainer books={books} /> : ''}
+            {spinner && <Spinner />}
+            {!spinner && books ? <CardContainer books={books} /> : ''}
 
         </div>
     )
